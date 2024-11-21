fb886e44741fbc3fbfb3d6f021badb4c861a81bb
Gautier Boder <gautier.boder@garaio.com>
2024-11-21 16:12:09 +0100

    added text translations to the API to realize some tests

--- src/api/api/Program.cs
+++ src/api/api/Program.cs
@@ -1,4 +1,8 @@
+using System.Globalization;
+using api;
 using Azure.Identity;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add localizations
+builder.Services.AddLocalization(options => options.ResourcesPath = "Texts");
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    var supportedCultures = new[]
+    {
+        "en-US", "fr-FR", "en", "fr", "de", "de-CH"
+    };
+
+    options.ApplyCurrentCultureToResponseHeaders = true;
+    options.SetDefaultCulture(supportedCultures[0])
+        .AddSupportedCultures(supportedCultures)
+        .AddSupportedUICultures(supportedCultures);
+});
+
 
 var app = builder.Build();
 
@@ -31,6 +51,8 @@ else if (!string.IsNullOrEmpty(vaultName) && !string.IsNullOrEmpty(clientId))
 
 app.UseHttpsRedirection();
 
+app.UseRequestLocalization(app.Services.GetRequiredService<IOptions<RequestLocalizationOptions>>().Value);
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
@@ -44,24 +66,36 @@ app.MapGet("/secret/{key}", async (string key) =>
 
 app.MapGet("/api", () => "Hello World!");
 
-app.MapGet("/api/health", () => new { Status = "Healthy", Hostname= Environment.MachineName });
+app.MapGet("/api/health", () => new { Status = "Healthy", Hostname = Environment.MachineName });
 
-app.MapGet("/api/version", () => new { Version = "1.0.0"});
+app.MapGet("/api/version", () => new { Version = "1.0.0" });
+
+app.MapGet("/api/text/", () => Texts.ResourceManager.GetResourceSet(CultureInfo.CurrentCulture, true, true));
+
+app.MapGet("/api/text/fixed", () =>
+    new
+    {
+        ResourceManager = Texts.ResourceManager.GetString("text02", CultureInfo.CurrentCulture),
+        Direct = Texts.text02
+    }
+);
+
+app.MapGet("/api/text/{text}", (string text) => Texts.ResourceManager.GetString(text, CultureInfo.CurrentCulture));
 
 app.MapGet("/api/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
 
 app.Run();
 
