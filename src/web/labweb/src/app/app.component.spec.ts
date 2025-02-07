553441a45bbf165cd8d46a0899097300b3dedd5b
Gautier Boder <gboder@outlook.com>
2025-02-07 09:45:10 +0100

    re-genrate application

--- src/web/labweb/src/app/app.component.spec.ts
+++ src/web/labweb/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'btweb' title`, () => {
+  it(`should have the 'labweb' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('btweb');
+    expect(app.title).toEqual('labweb');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, btweb');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, labweb');
   });
 });
